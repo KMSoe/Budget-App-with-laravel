d9eee0ccc4219764e7bb0a114d50289802b8d357
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2021-11-11 20:15:19 +0630

    Fix localization and budget model methods

--- app/Budget.php
+++ app/Budget.php
@@ -11,7 +11,7 @@ class Budget extends Model
 {
     static function getBudgetDetails($type, $month, $year, $total_amount)
     {
-        $budgetDetails = self::join('categories', 'budgets.category_id', '=', 'categories.id')->where('budgets.type', $type)->where("budgets.user_id", auth()->user()->id)->whereRaw('extract(month from budgets.created_at) = ?', [$month])->whereRaw('extract(year from budgets.created_at) = ?', [$year])->select(DB::raw('SUM(budgets.amount) AS amount'), 'budgets.category_id')->groupBy('budgets.category_id')->get();
+        $budgetDetails = Budget::join('categories', 'budgets.category_id', '=', 'categories.id')->where('budgets.type', $type)->where("budgets.user_id", auth()->user()->id)->whereRaw('extract(month from budgets.created_at) = ?', [$month])->whereRaw('extract(year from budgets.created_at) = ?', [$year])->select(DB::raw('SUM(budgets.amount) AS amount'), 'budgets.category_id')->groupBy('budgets.category_id')->get();
 
         $category_names = $category_amount_percentages = $category_colors = [];
 
@@ -34,9 +34,9 @@ class Budget extends Model
         $yearly_result = [];
         $brief = new stdClass();
         $brief->year = $year;
-        $brief->income = self::where('type', 'Income')->whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
-        $brief->expense = self::where('type', 'Expense')->whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
-        $brief->net_budget = self::whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
+        $brief->income = Budget::where('type', 'Income')->whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
+        $brief->expense = Budget::where('type', 'Expense')->whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
+        $brief->net_budget = Budget::whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
         $brief->percentage = number_format($brief->net_budget <= 0 ? 100 : abs(($brief->expense / $brief->income) * 100), 2, '.', ',');
         $yearly_result[] = $brief;
 
@@ -49,8 +49,8 @@ class Budget extends Model
 
         for ($i = 1; $i <= $month; $i++) {
             $income_amount = $expense_amount = 0;
-            $income_amount = self::where('type', 'Income')->whereRaw('extract(month from created_at) = ?', [$i])->whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
-            $expense_amount = self::where('type', 'Expense')->whereRaw('extract(month from created_at) = ?', [$i])->whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
+            $income_amount = Budget::where('type', 'Income')->whereRaw('extract(month from created_at) = ?', [$i])->whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
+            $expense_amount = Budget::where('type', 'Expense')->whereRaw('extract(month from created_at) = ?', [$i])->whereRaw('extract(year from created_at) = ?', [$year])->where('user_id', auth()->user()->id)->sum('amount');
 
             $res = new stdClass();
             $res->month = $stat->months[] = trans(date('M', mktime(0, 0, 0, $i, 10)));
